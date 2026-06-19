5bbbe3f5e047e788e4ff2c46b483197ccb19f329
Kübra Emektar <124355274+emektarkubra@users.noreply.github.com>
2026-06-19 14:19:21 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -37,8 +37,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "https://devcrew-web.vercel.app",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
